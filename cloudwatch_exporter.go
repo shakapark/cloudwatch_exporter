f37a9744666b65d941a3d98d324aa6207c2ad947
Shakapark <shakapark1994@gmail.com>
2018-03-05 22:25:32 +0100

    Change main.go by cloudwatch_exporter.go

--- cloudwatch_exporter.go
+++ cloudwatch_exporter.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/technofy/cloudwatch_exporter/config"
+	"github.com/shakapark/cloudwatch_exporter/config"
 	"os"
 	"sync"
 )
